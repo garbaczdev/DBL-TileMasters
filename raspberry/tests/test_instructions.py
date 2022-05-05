9cae51ce34106deda6fcd3593dcd66f88ae88227
garbaczmaciej <furn20102@gmail.com>
2022-05-05 18:56:34 +0200

    Instruction tests layout done

---
+++ raspberry/tests/test_instructions.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+
+class TestInstruction(TestCase):
+    pass
+
+
+class TestRequirementsInstruction(TestCase):
+    pass
+
+
+class TestBitmaskInstruction(TestCase):
+    pass
+
+
+class TestTileOrderInstruction(TestCase):
+    pass
