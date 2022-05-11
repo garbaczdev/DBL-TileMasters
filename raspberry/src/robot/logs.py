64bb5d05484be560c99608ec0982e88849360c9a
garbaczmaciej <furn20102@gmail.com>
2022-05-11 10:49:33 +0200

    logs.py commented

--- raspberry/src/robot/logs.py
+++ raspberry/src/robot/logs.py
@@ -5,7 +5,7 @@ from abc import ABC, abstractmethod
 from .config import Config as config
 
 
-# This is used for a log id. It is incremented every time the Log class is made.
+# This is used for a log id. It is increased by 1 every time the Log class is made.
 LOG_COUNTER = 0
 
 
@@ -15,7 +15,7 @@ class Log:
     """
     def __init__(self, component_name: str, _type: str, description: str, time: datetime, additional_data: dict = dict()) -> None:
         # Id of the log
-        self.id = self.get_id()
+        self.id = self._get_id()
         # Name of the component that created that log
         self.component_name = component_name
         # Type of the log: for now it can be either "error" or "action"
@@ -31,10 +31,16 @@ class Log:
         return f'[{self.id}]: ({self.get_date_as_str()}) [{self.component_name}]: {self.description}'
     
     def get_date_as_str(self) -> str:
+        """
+        Returns the log date as a string.
+        """
         return datetime.strftime(self.time, config.LOG_DATETIME_STR)
 
     @classmethod
-    def get_id(cls) -> None:
+    def _get_id(cls) -> None:
+        """
+        Returns the id of the log. Everytime this method is called it increases the counter by 1.
+        """
         global LOG_COUNTER
 
         id = LOG_COUNTER
@@ -44,27 +50,49 @@ class Log:
 
 
 class Logs:
+    """
+    Logs representation, which holds the info about actions and errors that occured in components.
+    """
 
     def __init__(self) -> None:
+        # List of logs.
         self._logs: list[Log] = list()
 
     def print(self, amount: int = 0) -> None:
+        """
+        Prints the logs. If given the amount, it will print last amount of logs.
+        If amount is 0, it will print all of the logs.
+        """
         for log in self._logs[-amount:]:
             print(log)
 
     def add(self, log: Log) -> None:
+        """
+        Adds a given log to the internal logs.
+        """
+        # If the logs should be used:
         if config.USE_LOGS:
+
+            # Add the log to the internal logs
             self._logs.append(log)
 
+            # If the logs should be printed
             if config.PRINT_LOGS:
+                # Print the logs.
                 print(log)
 
     def add_multiple(self, logs: list[Log]) -> None:
+        """
+        Add multiple logs to the internal logs.
+        """
         for log in logs:
             self.add_log(log)
 
 
 class LogComponent(ABC):
+    """
+    Abstract class for a component that can log its actions.
+    """
 
     def __init__(self, logs: Logs) -> None:
         self.logs = logs
@@ -72,20 +100,33 @@ class LogComponent(ABC):
     @property
     @abstractmethod
     def COMPONENT_NAME(self) -> str:
+        """
+        This should return the component name.
+        """
         pass
 
     def _log_action(self, description: str) -> None:
+        """
+        Saves the log of type "action" with the given description and current time.
+        """
         self._add_log("action", description)
 
     def _log_error(self, description: str) -> None:
+        """
+        Saves the log of type "error" with the given description and current time.
+        """
         self._add_log("error", description)
 
     def _add_log(self, _type: str, description: str) -> None:
+        """
+        Saves the log of given type, description and current time.
+        """
+        # Create a log object
         log = Log(
             self.COMPONENT_NAME,
             _type,
             description,
             datetime.now()
         )
-
+        # Save the log object
         self.logs.add(log)
