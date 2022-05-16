a41376c133078c16ce56aab7ab85350bc22e980b
Chloe Sandra Antonozzi <ch.antonozzi@gmail.com>
2022-05-16 16:30:21 +0200

    Fixed Bitmask

---
+++ raspberry/tests/test_tile_order_instruction.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+
+from src.robot.instructions import *
+from src.robot.config import Config as config
+from src.robot.exceptions import *
+
+class TestTileOrderInstruction(TestCase):
+    pass
