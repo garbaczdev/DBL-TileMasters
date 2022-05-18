d0985ff86a208f34bdee7c877f72507d33622436
Chloe Sandra Antonozzi <ch.antonozzi@gmail.com>
2022-05-18 10:00:43 +0200

    Requirements

--- raspberry/tests/test_tileorder_instruction.py
+++ raspberry/tests/test_tileorder_instruction.py
@@ -4,17 +4,16 @@ from src.robot.instructions import *
 from src.robot.config import Config as config
 from src.robot.exceptions import *
 
+
 class TestTileOrderInstruction(TestCase):
 
     def one_black_followed_by_one_white(self):
         instruction = TileOrderInstruction("10")
-        self.assertFalse(instruction.process(config.BLACk_TILE))
-        self.assertTrue(instruction.process(config.WHITE_TILE))
+        self.assertTrue(instruction.process(config.BLACK_TILE))
         self.assertFalse(instruction.process(config.WHITE_TILE))
     
     def two_black_followed_by_one_white(self):
         instruction = TileOrderInstruction("110")
-        self.assertFalse(instruction.process(config.BLACk_TILE))
-        self.assertTrue(instruction.process(config.WHITE_TILE))
+        self.assertTrue(instruction.process(config.BLACK_TILE))
+        self.assertTrue(instruction.process(config.BLACK_TILE))
         self.assertFalse(instruction.process(config.WHITE_TILE))
-#python3 -m unittest discover tests
