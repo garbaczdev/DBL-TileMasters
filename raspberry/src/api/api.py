ec62a0c124af3264a6764e3ac01dafa4accf1940
garbaczmaciej <furn20102@gmail.com>
2022-05-20 20:01:22 +0200

    Api skeleton done

---
+++ raspberry/src/api/api.py
@@ -0,0 +1,8 @@
+from ..robot import Robot
+
+class API:
+    def __init__(self, robot: Robot, port: int = 80) -> None:
+        pass
+
+    def run(self) -> None:
+        pass
--- raspberry/src/app.py
+++ raspberry/src/app.py
@@ -1,8 +1,10 @@
 from .robot import Robot
+from .api import API
 
 class App:
     def __init__(self) -> None:
         self.robot = Robot()
+        self.api = API(Robot, 80)
 
     def run(self) -> None:
         self.robot.run()
