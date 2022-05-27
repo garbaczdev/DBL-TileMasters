24d9b88c025238e963f1d0defec34f0f0009e1a7
SalaheldinM <104370167+SalaheldinM@users.noreply.github.com>
2022-05-28 00:19:24 +0200

    Update servo_motor.py

--- raspberry/components/servo_motor.py
+++ raspberry/components/servo_motor.py
@@ -1,13 +1,16 @@
-# Import libraries
+# https://nl.aliexpress.com/item/1005003697943326.html?gatewayAdapt=glo2nld
 import RPi.GPIO as GPIO
 import time
+
+
+gpio_pin = 11
 
 # Set GPIO numbering mode
 GPIO.setmode(GPIO.BOARD)
 
 # Set pin 11 as an output, and define as servo1 as PWM pin
-GPIO.setup(11,GPIO.OUT)
-servo1 = GPIO.PWM(11,50) # pin 11 for servo1, pulse 50Hz
+GPIO.setup(gpio_pin, GPIO.OUT)
+servo1 = GPIO.PWM(gpio_pin, 50) # pin 11 for servo1, pulse 50Hz
 
 # Start PWM running, with value of 0 (pulse off)
 servo1.start(0)
@@ -18,9 +21,9 @@ servo1.start(0)
 try:
     while True:
         #Ask user for angle and turn servo to it
-        angle = float(input('Enter angle between 0 & 180: '))
-        servo1.ChangeDutyCycle(2+(angle/18))
-        time.sleep(0.5)
+        duty_cycle = float(input('Enter duty cycle: '))
+        servo1.ChangeDutyCycle(duty_cycle)
+        time.sleep(2)
         servo1.ChangeDutyCycle(0)
 
 finally:
