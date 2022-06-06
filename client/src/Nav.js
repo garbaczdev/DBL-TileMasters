76016e26468985ea847cc24f6b71dea28202d898
garbaczmaciej <furn20102@gmail.com>
2022-06-06 11:33:28 +0200

    Nav working properly

--- client/src/Nav.js
+++ client/src/Nav.js
@@ -1,4 +1,8 @@
+import React from 'react';
+
 import {Link} from "react-router-dom";
+import { AiOutlineMenuFold,  AiOutlineMenuUnfold} from 'react-icons/ai';
+
 
 import './styles/Nav.css';
 
@@ -20,19 +24,43 @@ const subpages = [
 
 
 function Nav() {
+
+    const [hidden, setHidden] = React.useState(false);
+
+    function changeMenuShowing(){
+        setHidden(!hidden);
+    }
+
     return (
         <nav className="nav">
             <Link className="nav-logo" to="/">Home</Link>
-            <div className="nav-ul-div">
+            <div className={`nav-ul-div ${hidden ? "hidden":""}`}>
                 <ul className="nav-ul">
                     <li>Dark Mode</li>
                     {
                         subpages.map((subpage) => {
-                            return <li><Link key={subpage.url} to={subpage.url}>{subpage.title}</Link></li>
+                            return <li onClick={() => setHidden(true)}>
+                                <Link key={subpage.url} to={subpage.url}>{subpage.title}</Link>
+                            </li>
                         })
                     }
                 </ul>
             </div>
+            <div className="nav-menu-icon" onClick={changeMenuShowing}>
+                {
+                    hidden ? <AiOutlineMenuUnfold size={40}/> : <AiOutlineMenuFold size={40}/>
+                }
+            </div>
+            {/* {
+                hidden?
+                <li onClick={() => setHidden(false)}>
+                    <AiOutlineMenuUnfold/>
+                </li>
+                :
+                <li onClick={() => setHidden(true)}>
+                    <AiOutlineMenuFold/>
+                </li>
+            } */}
         </nav>
     );
   }
