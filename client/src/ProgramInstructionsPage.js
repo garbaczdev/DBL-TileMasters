1000145d80c7b31d0c924879710bfbad0d146d5b
garbaczmaciej <furn20102@gmail.com>
2022-06-12 21:11:26 +0200

    Before adding react-movable

--- client/src/ProgramInstructionsPage.js
+++ client/src/ProgramInstructionsPage.js
@@ -1,7 +1,25 @@
-function ProgramInstructionsPage() {
+import {Link} from "react-router-dom";
+
+import {getThemedIcon} from './icons';
+
+import './styles/ProgramInstructions.css';
+
+function ProgramInstructionsPage({darkTheme}) {
+
+    const Icon = getThemedIcon("ProgramInstructions", darkTheme);
+
     return (
         <div className="program-instructions-page subpage">
-            ProgramInstructionsPage
+            <Icon className="subpage-illustration"/>
+            <h1 className="subpage-title">Program Instructions</h1>
+            <p className="subpage-description">Program the robot's behavior</p>
+            
+
+            
+            <label className='std-label'>Need an explaination?</label>
+            <Link to="./tutorial">
+                <div className="std-btn">Open Tutorial</div>
+            </Link>
         </div>
     );
   }
