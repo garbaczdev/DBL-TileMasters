b0b8d8617fcea7e7b848a51bad7349a5ab9f6c5c
garbaczmaciej <furn20102@gmail.com>
2022-06-14 13:40:32 +0200

    Programming popup semi-done

--- client/src/ProgramInstructionsPage.js
+++ client/src/ProgramInstructionsPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 import {Link} from "react-router-dom";
+import Popup from 'reactjs-popup';
 
 import {getThemedIcon, getIcon} from './icons';
 import StdItem from './StdItem';
+import { sendInstructions as apiSendInstructions} from './apiUtils';
 
 import './styles/ProgramInstructions.css';
 
@@ -20,7 +22,7 @@ const instructionAttributes = [
 ]
 
 
-function Instruction({instructionJson}){
+function Instruction({instructionJson, index, deleteCallback}){
 
     let Icon = null;
     let name = null;
@@ -90,6 +92,63 @@ function Instruction({instructionJson}){
     );
 }
 
+class ProgramInstructionsPopup extends React.Component{
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            instructionType: "requirements",
+            instructionOptions: {
+                white: 0,
+                black: 0
+            },
+            repeat: 1
+        }
+    }
+
+    render(){
+        return (
+            <Popup 
+                open={this.props.parent.isPopupOpened()}
+                onClose={() => this.props.parent.closePopup()}
+                position="right center"
+                className="program-instructions-popup"
+            >
+                <label className='std-label'>Instruction Type:</label>
+                <select className='std-dropdown' value={this.state.instructionType}
+                    onChange={e => this.setState({...this.state, instructionType: e.target.value})}
+                >
+                    <option value="requirements">Requirements</option>
+                    <option value="pattern">Pattern</option>
+                    <option value="bitmask">Bitmask</option>
+                </select> 
+
+
+                <label className='std-label'>Number of repetitions:</label>
+                <input className="std-text-input number-input" title="Number of repetitions" value={this.state.repetitions} 
+                    onChange={e => this.setState({...this.state, repetitions: e.target.value})}
+                />
+
+                <div className='btn-div'>
+
+                    <button className='std-btn' onClick={() => this.props.parent.closePopup()}>Close</button>
+
+                    {
+                        this.canBeAdded() && <button className='std-btn' onClick={() => this.props.parent.closePopup()}>Add Instruction</button>
+                    }
+
+                </div>
+            </Popup>
+        );
+    }
+
+    canBeAdded(){
+        return true;
+    }
+
+}
+
 
 class InstructionList extends React.Component{
 
@@ -107,28 +166,50 @@ class InstructionList extends React.Component{
                 "type": "bitmask",
                 "bitmask": "1",
                 "repetitions": -1
-            }]
+            }],
+            popupOpen: false
         }
     }
 
     render(){
         return (
-            <ul className='std-item-list'>
-                {
-                    this.state.instructionsJson.map((instructionJson, index, instructionsJson) => 
-                        <Instruction key={index} instructionJson={instructionJson} moveCallback={this.moveCallback} index={index} length={instructionsJson.length}/>
-                    )
-                }
-            </ul>
+            <>
+                <ul className='std-item-list'>
+                    {
+                        this.state.instructionsJson.map((instructionJson, index, instructionsJson) => 
+                            <Instruction key={index} instructionJson={instructionJson} index={index} deleteCallback={this.deleteCallback}/>
+                        )
+                    }
+                </ul>
+                <div className="btn-div">
+                    <button className='std-btn' onClick={() => this.openPopup()}>Add Instruction</button>
+                    <button className='std-btn' onClick={() => this.sendInstructions()}>Send Instructions</button>
+                </div>
+
+                <ProgramInstructionsPopup parent={this}/>
+            </>
         );
     }
 
-    moveCallback(index, up=true){
+    isPopupOpened(){
+        return this.state.popupOpen;
+    }
+
+
+    openPopup(){
+        this.setState({...this.state, popupOpen: true})
+    }
+
+    closePopup(){
+        this.setState({...this.state, popupOpen: false})
+    }
+
+    deleteCallback(index){
 
     }
 
-    indexCallback(){
-
+    sendInstructions(){
+        apiSendInstructions(this.state.instructionsJson);
     }
 
 }
